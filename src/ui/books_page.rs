31c1e22ca4a263647ed83e20d73fa2d28a724cd9
Max Hackinger <mahaxi@gmail.com>
2021-11-03 17:08:15 -0400

    generate initial test covers in book view

--- src/ui/books_page.rs
+++ src/ui/books_page.rs
@@ -1,9 +1,14 @@
-use glib::{clone, Sender};
+use log::debug;
+
+use glib::{clone, Sender, MainContext};
+use glib::PRIORITY_DEFAULT;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::CompositeTemplate;
 use gtk::{gio, glib};
 use once_cell::unsync::OnceCell;
+
+use openlibrary_client::{Edition, Client};
 
 use crate::application::Action;
 use crate::ui::book_cover;
@@ -69,8 +74,33 @@ impl BooksPage {
 
     fn setup_widget(&self, sender: Sender<Action>) {
         let imp = imp::BooksPage::from_instance(self);
-        let cover = book_cover::BookCover::new();
+        let main_context = MainContext::default();
 
-        imp.books_flowbox.insert(&cover, -1)
+        let books_flowbox: gtk::FlowBox = imp.books_flowbox.clone().downcast().unwrap();
+        
+        main_context.spawn_local(clone!(@weak books_flowbox => async move {
+            let client = Client::new();
+            let client2 = Client::new();
+            
+            let entity = client.entity_by_isbn("9781849352826").await;
+            let entity2 = client2.entity_by_isbn("9781849352826").await;
+
+
+            match entity {
+                Ok(entity) => {
+                    let cover = book_cover::BookCover::new(entity);
+                    books_flowbox.insert(&cover, -1);
+                }
+                Err(error) => debug!("Failed to parse entity form ol: {}", error),
+            };
+            
+            match entity2 {
+                Ok(entity2) => {
+                    let cover2 = book_cover::BookCover::new(entity2);
+                    books_flowbox.insert(&cover2, -1);
+                }
+                Err(error) => debug!("Failed to parse entity form ol: {}", error),
+            };
+        }));
     }
 }
