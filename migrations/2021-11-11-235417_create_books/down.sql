05231dc10205313ee4d6d974e491af6243353acc
Max Hackinger <mahaxi@gmail.com>
2021-11-16 16:23:44 -0500

    inital simple sql

--- migrations/2021-11-11-235417_create_books/down.sql
+++ migrations/2021-11-11-235417_create_books/down.sql
@@ -1,1 +1,6 @@
 -- This file should undo anything in `up.sql`
+DROP TABLE books;
+
+DROP TABLE authors;
+
+DROP TABLE works;
--- migrations/2021-11-11-235417_create_books/up.sql
+++ migrations/2021-11-11-235417_create_books/up.sql
@@ -1,1 +1,23 @@
 -- Your SQL goes here
+CREATE TABLE `books` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `olid` TEXT NOT NULL,
+    `uid` TEXT UNIQUE,
+    `title` TEXT NOT NULL,
+    `author` TEXT,
+    `work` TEXT
+);
+
+CREATE TABLE authors (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `olid` TEXT NOT NULL,
+    `uid` TEXT UNIQUE,
+    `name` TEXT NOT NULL
+);
+
+CREATE TABLE works (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `olid` TEXT NOT NULL,
+    `title` TEXT NOT NULL,
+    `author` TEXT
+);
