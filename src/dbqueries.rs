d9a663da72e11fcf3a8beb0a9440389e969580ab
Max Hackinger <mahaxi@gmail.com>
2021-12-01 18:27:21 -0500

    add query to enter a new book in db

--- src/dbqueries.rs
+++ src/dbqueries.rs
@@ -1,7 +1,9 @@
 use diesel::prelude::*;
 
+use openlibrary_client::Entity;
+
 use crate::database;
-use crate::models::{Author, Book, Work};
+use crate::models::{Author, Book, Work, NewAuthor, NewBook, NewWork};
 use crate::schema::{authors, books, works};
 
 pub fn books() -> Result<Vec<Book>, diesel::result::Error> {
@@ -12,7 +14,20 @@ pub fn books() -> Result<Vec<Book>, diesel::result::Error> {
     Ok(books)
 }
 
-pub fn add_book() {}
+pub fn add_book(book: &Entity, uid: String) {
+    let connection = database::connection().get().unwrap();
+
+    let book: NewBook = NewBook {
+        olid: &book.get_olid(),
+        uid: &uid,
+        title: &book.get_edition().title,
+        author: &book.get_author_name(),
+        work: &book.get_work().key,
+        covers: &book.get_edition().covers[0].to_string(),
+    };
+
+    diesel::insert_into(books::table).values(&book).execute(&connection).expect("Error saving book");
+}
 
 pub fn work() {}
 
